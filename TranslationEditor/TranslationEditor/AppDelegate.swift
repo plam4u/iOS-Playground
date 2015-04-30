75c9a312443420b084621f815cd0524f640d4be2
Plamen Andreev <plam4u@gmail.com>
2015-04-30 08:55:49 +0100

    added TranslationEditor

---
+++ TranslationEditor/TranslationEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TranslationEditor
+//
+//  Created by Plamen Andreev on 29/4/15.
+//  Copyright (c) 2015 Plamen Andreev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TranslationEditor/TranslationEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TranslationEditor
+//
+//  Created by Plamen Andreev on 29/4/15.
+//  Copyright (c) 2015 Plamen Andreev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
