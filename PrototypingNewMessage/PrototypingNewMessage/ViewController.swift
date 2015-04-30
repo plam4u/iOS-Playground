cad86ff201df8e84b7575048aab01a3c7cf06d17
Plamen Andreev <plam4u@gmail.com>
2015-04-30 08:55:34 +0100

    added PrototypingNewMessage

---
+++ PrototypingNewMessage/PrototypingNewMessage/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PrototypingNewMessage
+//
+//  Created by Plamen Andreev on 29/4/15.
+//  Copyright (c) 2015 Plamen Andreev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var propertyAddressLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
